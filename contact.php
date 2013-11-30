ae070ec2d3e2af30d7734779b817b206ad18d4eb
Synchromatic <weiwei.cheng@rule14.com>
2013-11-29 22:39:54 -0800

    Overhaul

--- contact.php
+++ contact.php
@@ -1,51 +1,123 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Home Page</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Bootstrap -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Wei-Wei Cheng</title>
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 <![endif]-->
-</head>
-<body>
 
-<nav class="navbar navbar-default" role="navigation">
-<!-- Brand and toggle get grouped for better mobile display -->
+<!-- Custom styles for this template -->
+<link href="carousel.css" rel="stylesheet">
+</head>
+<!-- NAVBAR
+================================================== -->
+<body>
+<div class="navbar-wrapper">
+<div class="container">
+
+<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+<div class="container">
 <div class="navbar-header">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 <span class="sr-only">Toggle navigation</span>
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="index.php">Wei-Wei Cheng</a>
+<a class="navbar-brand" href="#">Wei-Wei Cheng</a>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav">
+<li class="active"><a href="#">Home</a></li>
+<li><a href="#about">About</a></li>
+<li><a href="#contact">Contact</a></li>
+</ul>
+</div>
+</div>
 </div>
 
-<!-- Collect the nav links, forms, and other content for toggling -->
-<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-<ul class="nav navbar-nav navbar-right">
-<li><a href="#">About Me</a></li>
-<li><a href="#">Contact</a></li>
+</div>
+</div>
 
-</ul>
-</li>
-</ul>
-</div><!-- /.navbar-collapse -->
-</nav>
 
-<center>
-<h1><bold>I can be reached in the following ways: </bold></h1>
-<bold>Email:</bold> cheng[dot]2wei[at]gmail[dot].com
-</center>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-             <script src="https://code.jquery.com/jquery.js"></script>
-             <!-- Include all compiled plugins (below), or include individual files as needed -->
-             <script src="js/bootstrap.min.js"></script>
-             </body>
-             </html>
+
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing">
+
+<!-- START THE FEATURETTES -->
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-7">
+<h2 class="featurette-heading">Software meets Hardware<span class="text-muted">It'll blow your mind.</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<div class="col-md-5">
+<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-5">
+<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+<div class="col-md-7">
+<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-7">
+<h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<div class="col-md-5">
+<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<!-- /END THE FEATURETTES -->
+
+
+<!-- FOOTER -->
+<footer>
+<p class="pull-right"><a href="#">Back to top</a></p>
+<p>&copy; 2013 Wei-Wei Cheng &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+
+</div><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/holder.js"></script>
+</body>
+</html>
