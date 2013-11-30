b86285ede1028c02dc8d3e2eeefbb81dfcc76d2f
Synchromatic <weiwei.cheng@rule14.com>
2013-11-29 22:02:36 -0800

    Self

--- index.php
+++ index.php
@@ -43,6 +43,7 @@
 <ul class="nav navbar-nav">
 <li class="active"><a href="#">Home</a></li>
 <li><a href="#about">About</a></li>
+<li><a href="#contact">Contact</a></li>
 </ul>
 </div>
 </div>
@@ -63,12 +64,11 @@
 </ol>
 <div class="carousel-inner">
 <div class="item active">
-<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide">
+<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:Hello" alt="First slide">
 <div class="container">
 <div class="carousel-caption">
-<h1>Example headline.</h1>
-<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+<h1>Hi, my name is Wei-Wei.</h1>
+<p>You are looking at my website.</p>
 </div>
 </div>
 </div>
@@ -78,7 +78,6 @@
 <div class="carousel-caption">
 <h1>Another example headline.</h1>
 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
 </div>
 </div>
 </div>
@@ -88,7 +87,6 @@
 <div class="carousel-caption">
 <h1>One more for good measure.</h1>
 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
 </div>
 </div>
 </div>
